943e40db7d2a9664d3ebbcaf0d802fc78287f34d
Emils <emilsberzs@hotmail.com>
2023-02-28 11:00:24 +0000

    Created seperate module for DOM manipulation page_builder.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import { firstLoad } from './page_builder'
 import './styles.css'
 import { allTasks, makeToDo, findIndex, removeTask, toggleComplete } from './task_manager'
-
-
 
 //Just arbitrary tasks for now
 const task_one = new makeToDo('work', 'go to work', 'today', 'high')
 const task_two = new makeToDo('eat', 'have some breakfast', 'tommorow', 'medium')
 const task_three = new makeToDo('sleep', 'go to bed', 'today', 'low')
+
+firstLoad()
---
+++ src/page_builder.js
@@ -0,0 +1,9 @@
+export const sidebar = document.getElementById('sidebar');
+export const workspace = document.getElementById('workspace')
+
+
+export function firstLoad() {
+    sidebar.innerHTML = 'test'
+    workspace.innerHTML = 'test'
+
+}
